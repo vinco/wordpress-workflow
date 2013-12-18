961adf09ddfb998fba8bcd94a175869ec7cbb83b
Héctor Gutiérrez <haga87@gmail.com>
2013-12-18 15:59:09 -0600

    Se agrega la dirección por default

---
+++ www/default/index.php
@@ -0,0 +1,36 @@
+<?php
+/* If a custom dashboard file exists, load that instead of the default
+ * dashboard provided by Varying Vagrant Vagrants. This file should be
+ * located in the `www/default/` directory.
+ */
+if ( file_exists( 'dashboard-custom.php' ) ) {
+        include( 'dashboard-custom.php' );
+        exit;
+}
+
+// Begin default dashboard.
+?>
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Varying Vagrant Vagrants Dashboard</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<ul class="nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="http://github.com/10up/varying-vagrant-vagrants">Repository</a></li>
+        <li><a href="database-admin/">phpMyAdmin</a></li>
+        <li><a href="memcached-admin/">phpMemcachedAdmin</a></li>
+        <li><a href="webgrind/">Webgrind</a></li>
+        <li><a href="phpinfo/">PHP Info</a></li>
+</ul>
+
+<ul class="nav">
+        <li><a href="http://local.wordpress.dev/">http://local.wordpress.dev</a> for WordPress stable</li>
+        <li><a href="http://local.wordpress-trunk.dev/">http://local.wordpress-trunk.dev</a> for WordPress trunk</li>
+        <li><a href="http://src.wordpress-develop.dev/">http://src.wordpress-develop.dev</a> for trunk WordPress development files</li>
+        <li><a href="http://build.wordpress-develop.dev/">http://build.wordpress-develop.dev</a> for a Grunt built of those development files</li>
+</ul>
+</body>
+</html>
