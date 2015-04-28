26adf2ced187e3604b268bd5f12deeadc1cef38b
Héctor Gutiérrez <haga87@gmail.com>
2015-04-28 10:49:57 -0500

    (md:91563) Fixes general form of change_domain

--- documentation/commands.php
+++ documentation/commands.php
@@ -208,16 +208,12 @@ $ fab environment:vagrant <strong>install_plugins</strong>
                             <code>vagrant</code> environment.
                         </p>
                         <pre>
-$ fab environment:env_name[,debug] <strong>change_domain</strong>
+$ fab environment:vagrant <strong>change_domain</strong>
                         </pre>
 
                         <h4>Arguments</h4>
                         <p>None</p>
 
-                        <h4>Examples</h4>
-                        <pre>
-$ fab environment:vagrant <strong>change_domain</strong>
-                        </pre>
                     </div>
 
                     <div class="col-lg-12" id="import_data" name="import_data">
